c596760a657b1b66533523ebc16467dec92729ba
oArmando <armands.ostrovskis7@gmail.com>
2020-11-25 11:46:30 +0200

    Herokuu app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have edited <code>src/App.js</code> and made app
         </p>
         <a
           className="App-link"
